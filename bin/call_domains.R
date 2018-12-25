89ffc85f6341ad5308766e398e43dac456f3640b
Shi Binbin <ltbyshi@gmail.com>
2018-12-26 01:14:34 +0800

    Added call_domains.R

---
+++ bin/call_domains.R
@@ -0,0 +1,35 @@
+#! /usr/bin/env Rscript
+library(countreg)
+
+args <- commandArgs(trailingOnly=TRUE)
+if(length(args) != 3) {
+    message("Usage: ", args[1], " input_file fdr")
+    Sys.exit(1)
+}
+bed_file <- args[2]
+fdr_value <- as.numeric(args[3])
+
+bed <- read.table(bed_file, sep='\t', header=FALSE)
+x <- as.integer(bed[, 5])
+
+ztnb.test <- function(x, mu, theta) {
+    # build a p-value table
+    max_x = qztnbinom(1 - 1e-10, mu=mu, theta=theta)
+    pvalue_table <- dztnbinom(1:max_x, mu=mu, theta=theta)
+    pvalue_table <- rev(cumsum(rev(pvalue_table)))
+    # calculate pvalues
+    pvalues <- rep(1e-10, length(x))
+    pvalues[x <= max_x] <- pvalue_table[x[x <= max_x]]
+    qvalues <- p.adjust(pvalues, method='BH')
+    return(qvalues)
+}
+
+bg = x[x < quantile(x, 0.99)]
+fit <- zerotrunc(bg ~ 1, dist='negbin')
+qvalues <- ztnb.test(x, mu=exp(fit$coefficients), theta=fit$theta)
+bg <- x[qvalues >= fdr_value]
+fit <- zerotrunc(bg ~ 1, dist='negbin')
+qvalues <- ztnb.test(x, mu=exp(fit$coefficients), theta=fit$theta)
+bed_sig <- bed[qvalues < fdr_value,]
+bed_sig <- cbind(bed_sig, qvalues[qvalues < fdr_value])
+write.table(bed_sig, row.names=FALSE, col.names=FALSE, sep='\t', quote=FALSE)
