11b09df730cb0b96f1548fcb192563b9591251b2
Shi Binbin <ltbyshi@gmail.com>
2018-12-26 11:34:36 +0800

    Added message in call_domains.R

--- bin/call_domains.R
+++ bin/call_domains.R
@@ -2,14 +2,18 @@
 library(countreg)
 
 args <- commandArgs(trailingOnly=TRUE)
-if(length(args) != 3) {
-    message("Usage: ", args[1], " input_file fdr")
-    Sys.exit(1)
+if(length(args) != 2) {
+    message("Usage: call_domains.R input_file fdr")
+    quit(save='no', status=1)
 }
-bed_file <- args[2]
-fdr_value <- as.numeric(args[3])
+bed_file <- args[1]
+fdr_value <- as.numeric(args[2])
 
+message('read coverage file:', bed_file)
 bed <- read.table(bed_file, sep='\t', header=FALSE)
+message('numbe of bins:', nrow(bed))
+# remove bins with 1 read
+bed <- bed[bed[, 5] > 1,]
 x <- as.integer(bed[, 5])
 
 ztnb.test <- function(x, mu, theta) {
@@ -25,11 +29,15 @@ ztnb.test <- function(x, mu, theta) {
 }
 
 bg = x[x < quantile(x, 0.99)]
+message('fit ZTNB distribution')
 fit <- zerotrunc(bg ~ 1, dist='negbin')
 qvalues <- ztnb.test(x, mu=exp(fit$coefficients), theta=fit$theta)
 bg <- x[qvalues >= fdr_value]
+message('fit ZTNB distribution')
 fit <- zerotrunc(bg ~ 1, dist='negbin')
 qvalues <- ztnb.test(x, mu=exp(fit$coefficients), theta=fit$theta)
 bed_sig <- bed[qvalues < fdr_value,]
 bed_sig <- cbind(bed_sig, qvalues[qvalues < fdr_value])
+message('number of significant bins:', nrow(bed_sig))
+message('write significant domains')
 write.table(bed_sig, row.names=FALSE, col.names=FALSE, sep='\t', quote=FALSE)
