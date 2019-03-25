3a2db66241ed53bff1ea364ef1d91ed9ac3059ce
Shi Binbin <ltbyshi@gmail.com>
2019-03-25 11:32:33 +0800

    Added refine_peaks

--- bin/call_peak.py
+++ bin/call_peak.py
@@ -93,14 +93,17 @@ def estimate_bg_global(signals):
     bg = np.median(signals_mean)
     return bg
 
-def call_peaks(signals, min_length=None):
-    bg_global = estimate_bg_global(signals)
+def call_peaks(signals, min_length=2):
+    #bg_global = estimate_bg_global(signals)
+    bg_global = None
     peaks = []
     for i, signal in enumerate(signals):
         peak_locations = call_peak_gene(signal, bg_global=bg_global, smooth=True)
+        #print(signal)
         for start, end in peak_locations:
-            if (min_length is not None) and ((end - start) >= min_length):
-                peaks.append((i, start, end))
+            #print(peak_locations)
+            if (min_length is None) or ((end - start) >= min_length):
+                peaks.append((i, start, end, signal[start:end].mean()))
     return peaks
 
 @command_handler
@@ -124,9 +127,97 @@ def call_peak(args):
                 fout.write('{}\t{}\t{}\n'.format(gene_ids[i], peaks_locations[j, 0], peaks_locations[j, 1]))
 
 @command_handler
-def refine_peak_boundary(args):
-    pass
-    
+def refine_peaks(args):
+    import pandas as pd
+    import numpy as np
+    from tqdm import tqdm
+    import re
+    import h5py
+    #from bx.bbi.bigwig_file import BigWigFile
+    from pykent import BigWigFile
+    from ioutils import open_file_or_stdout
+
+
+    logger.info('read input matrix file: ' + args.peaks)
+    #matrix = pd.read_table(args.matrix, sep='\t', index_col=0)
+    #feature_info = matrix.index.to_series().str.split('|', expand=True)
+    #feature_info.columns = ['gene_id', 'gene_type', 'gene_name', 'domain_id', 'transcript_id', 'start', 'end']
+    #feature_info['start'] = feature_info['start'].astype('int')
+    #feature_info['end'] = feature_info['end'].astype('int')
+    peaks = pd.read_table(args.peaks, sep='\t', header=None, dtype=str)
+    if peaks.shape[1] < 6:
+        raise ValueError('less than 6 columns in peak file')
+    peaks.columns = ['chrom', 'start', 'end', 'name', 'score', 'strand'] + ['c%d'%i for i in range(6, peaks.shape[1])]
+    peaks['start'] = peaks['start'].astype('int')
+    peaks['end'] = peaks['end'].astype('int')
+
+    logger.info('read chrom sizes: ' + args.chrom_sizes)
+    chrom_sizes = pd.read_table(args.chrom_sizes, sep='\t', header=None, names=['chrom', 'size'])
+    chrom_sizes = chrom_sizes.drop_duplicates('chrom')
+    chrom_sizes = chrom_sizes.set_index('chrom').iloc[:, 0]
+
+    logger.info('read input genomic bigwig file: ' + args.tbigwig)
+    tbigwig = BigWigFile(args.tbigwig)
+    #chrom_sizes.update(dict(tbigwig.get_chrom_sizes()))
+
+    gbigwig = {}
+    logger.info('read input genomic bigwig (+) file: ' + args.gbigwig_plus)
+    gbigwig['+'] = BigWigFile(args.gbigwig_plus)
+    logger.info('read input genomic bigwig (-) file: ' + args.gbigwig_minus)
+    gbigwig['-'] = BigWigFile(args.gbigwig_minus)
+    #chrom_sizes.update(dict(gbigwig['+'].get_chrom_sizes()))
+
+    flanking = args.flanking
+    signals = []
+    signals_mean = []
+    windows = []
+    pat_gene_id = re.compile('^(.*)_([0-9]+)_([0-9]+)_([+-])$')
+    for _, peak in peaks.iterrows():
+        if peak['chrom'].startswith('chr'):
+        #if feature['gene_type'] == 'genomic':
+            #chrom, start, end, strand = pat_gene_id.match(feature['gene_id']).groups()
+            #start = int(start)
+            #end = int(end)
+            window_start = max(0, peak['start'] - flanking)
+            window_end = min(peak['end'] + flanking, chrom_sizes[peak['chrom']])
+            data = gbigwig[peak['strand']].query_interval(peak['chrom'], window_start, window_end, fillna=0)
+        else:
+            strand = '+'
+            window_start = max(0, peak['start'] - flanking)
+            window_end = min(peak['end'] + flanking, chrom_sizes[peak['chrom']])
+            data = tbigwig.query_interval(peak['chrom'], window_start, window_end, fillna=0)
+        if data is None:
+            data = np.zeros((window_end - window_start))
+            #logger.info('no coverage data found for peak: {}'.format(feature['domain_id']))
+        if args.use_log:
+            data = np.log2(np.maximum(data, 0.25)) + 2
+        signals.append(data)
+        signals_mean.append(np.mean(data))
+        windows.append((peak['chrom'], window_start, window_end, peak['start'], peak['end'], peak['strand']))
+    windows = pd.DataFrame.from_records(windows)
+    windows.columns = ['chrom', 'window_start', 'window_end', 'start', 'end', 'strand']
+
+    logger.info('call peaks')
+    refined_peaks = call_peaks(signals, min_length=args.min_length)
+    with open_file_or_stdout(args.output_file) as fout:
+        for i, start, end, mean_signal in refined_peaks:
+            # map peak coordinates from window to original
+            peak = [windows['chrom'][i], 
+                start + windows['start'][i],
+                end + windows['start'][i],
+                'peak_%d'%(i + 1),
+                '%.4f'%mean_signal,
+                strand
+            ]
+            # remove peaks not overlapping with the window
+            if (peak[1] > windows['end'][i]) or (peak[2] < windows['start'][i]):
+                continue
+            fout.write('\t'.join(map(str, peak)) + '\n')
+        #print('%s\t%d\t%d => %s\t%d\t%d'%(
+        #    windows['chrom'][i], windows['start'][i], windows['end'][i],
+        #    windows['chrom'][i], start, end))
+
+
 if __name__ == '__main__':
     main_parser = argparse.ArgumentParser(description='Call peaks from exRNA signals')
     subparsers = main_parser.add_subparsers(dest='command')
@@ -144,6 +235,30 @@ if __name__ == '__main__':
         help='weight for local background (0.0-1.0)')
     parser.add_argument('--output-file', '-o', type=str, required=True,
         help='output plot file BED format')
+    
+    parser = subparsers.add_parser('refine_peaks')
+    parser.add_argument('--peaks', type=str, required=True,
+        help='input count matrix with feature names as the first column')
+    parser.add_argument('--tbigwig', type=str, required=True,
+        help='transcript BigWig file')
+    parser.add_argument('--gbigwig-plus', type=str, required=True,
+        help='genomic BigWig (+) file')
+    parser.add_argument('--gbigwig-minus', type=str, required=True,
+        help='genomic BigWig (-) file')
+    parser.add_argument('--chrom-sizes', type=str, required=True,
+        help='chrom sizes')
+    parser.add_argument('--output-file', '-o', type=str, default='-',
+        help='output refined peaks')
+    parser.add_argument('--use-log', action='store_true', 
+        help='use log10 instead raw signals')
+    parser.add_argument('--smooth', action='store_true',
+        help='merge adjacent peaks')
+    parser.add_argument('--local-bg-width', type=int, default=3,
+        help='number of nearby bins for estimation of local background')
+    parser.add_argument('--local-bg-weight', type=float, default=0.5, 
+        help='weight for local background (0.0-1.0)')
+    parser.add_argument('--flanking', type=int, default=20)
+    parser.add_argument('--min-length', type=int, default=10)
 
     args = main_parser.parse_args()
     if args.command is None:
